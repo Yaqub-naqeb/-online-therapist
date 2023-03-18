8c7aed4c9b98a56759d9e36ed03c5a424eb18a0d
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-03-19 02:03:27 +0300

    signUp design Page

--- online_therapist/routes/web.php
+++ online_therapist/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/signup', function () {
+    return view('signup',[
+        'title'=>'signup Now'
+    ]);
+});
+
+
