ff1292e027ea7034ec62247c627b1de31d9326bf
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-04-08 03:24:10 +0300

    setup the authentication

--- online_therapist/routes/web.php
+++ online_therapist/routes/web.php
@@ -1,6 +1,26 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,31 +33,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/signup', function () {
-    return view('signup',[
-        'title'=>'signup Now'
-    ]);
-});
-// login
-Route::get('/login', function () {
-    return view('login',[
-        'title'=>'Login'
-    ]);
-});
-// blogs
-Route::get('/blogs', function () {
-    return view('blogs',[
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/signup', function () {
+//     return view('signup',[
+//         'title'=>'signup Now'
+//     ]);
+// });
+// // login
+// Route::get('/login', function () {
+//     return view('login',[
+//         'title'=>'Login'
+//     ]);
+// });
+// // blogs
+// Route::get('/blogs', function () {
+//     return view('blogs',[
         
-    ]);
-});
-// After signup
-Route::get('/aftersignup', function () {
-    return view('aftersignup',[
+//     ]);
+// });
+// // After signup
+// Route::get('/aftersignup', function () {
+//     return view('aftersignup',[
         
-    ]);
-});
-
-
+//     ]);
+// });
