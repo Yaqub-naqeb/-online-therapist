013d797385eba5ce28ccf5703e8f2e5c1150607a
Abdulbari <abdulbari.112917@gmail.com>
2023-04-12 23:06:13 +0300

    error fixing

--- online_therapist/routes/web.php
+++ online_therapist/routes/web.php
@@ -3,6 +3,16 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,43 +29,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-// Route::get('/signup', function () {
-//     return view('signup',[
-//         'title'=>'signup Now'
-//     ]);
-// });
-// // login
-// Route::get('/login', function () {
-//     return view('login',[
-//         'title'=>'Login'
-//     ]);
-// });
-// // blogs
-// Route::get('/blogs', function () {
-//     return view('blogs',[
-        
-//     ]);
-// });
-// // After signup
-// Route::get('/aftersignup', function () {
-//     return view('aftersignup',[
-        
-//     ]);
-// });
