46272b552588f0916168c17403385fdc7d61e97d
Abdulbari <abdulbari.112917@gmail.com>
2023-04-18 18:26:42 +0300

    fixing errors

--- online_therapist/resources/views/about.blade.php
+++ online_therapist/resources/views/about.blade.php
@@ -1,50 +1,55 @@
 @extends('layout')
 @section('content')
-<div class="bd">
+
+<section>
     <link rel="stylesheet" href="{{asset('style/about.css')}}">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-
-    <div class="wrap">
-        <h1 class="title">{{$title}}</h1>
-        <p class="meet">{{$meet}}</p>
+    <div class="bd">
+      
+      
     
-      </div>
-        <div class="allCards">
-    
-    {{-- first Card --}}
-            <div class="containar">
-                <div class="imcont">
-                    <img src="{{asset('images/Yaqwb.JPG')}}" class="image" alt="">
-            
+        <div class="wrap">
+            <h1 class="title">{{$title}}</h1>
+            <p class="meet">{{$meet}}</p>
+        
+          </div>
+            <div class="allCards">
+        
+        {{-- first Card --}}
+                <div class="containar">
+                    <div class="imcont">
+                        <img src="{{asset('images/Yaqwb.JPG')}}" class="image-about" alt="">
+                
+                    </div>
+                {{-- writing --}}
+                <div class="bottom ">
+                    <p class="name">Yaqwb Naqib </p>
+                    <p class="exp">Software developer</p>
                 </div>
-            {{-- writing --}}
-            <div class="bottom ">
-                <p class="name">Yaqwb Naqib </p>
-                <p class="exp">Software developer</p>
-            </div>
-            </div>
-    
-    
-    {{-- Second Card --}}
-    
-            <div class="containar">
-                <div class="imcont">
-                    <img src="{{asset('images/myimg.JPG')}}" class="image" alt="">
-            
                 </div>
-            {{-- writing --}}
-            <div class="bottom ">
-                <p class="name">Abdulbari Qaisar</p>
-                <p class="exp">Software developer</p>
-            </div>
-            </div>
+        
+        
+        {{-- Second Card --}}
+        
+                <div class="containar">
+                    <div class="imcont">
+                        <img src="{{asset('images/myimg.JPG')}}" class="image-about" alt="">
+                
+                    </div>
+                {{-- writing --}}
+                <div class="bottom ">
+                    <p class="name">Abdulbari Qaisar</p>
+                    <p class="exp">Software developer</p>
+                </div>
+                </div>
+        
+        
+        </div>
+        
     
     
-    </div>
-    
+     </div>
+</section>
 
-
- </div>
 
 @endsection
  
