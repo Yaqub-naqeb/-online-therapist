a6278a2a35166a38ed27c35ca90bcf2883b80b26
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-04-22 16:26:00 +0300

    style of contact us page completed

--- online_therapist/resources/views/contactUs.blade.php
+++ online_therapist/resources/views/contactUs.blade.php
@@ -6,26 +6,26 @@
 
     {{-- header --}}
    <div class="cont">
-   <h1>{{$title}}</h1>
-   <p>{{$p}}</p>
+   <h1 class="h1">{{$title}}</h1>
+   <p class="p">{{$p}}</p>
    </div>
    {{-- form --}}
    <form action="" class="form">
     <div class="cont_1">
         <p>FullName:</p>
-        <input class="inp_1" type="text" placeholder="Enter your fullName here...">
+        <input class="inp_1 inpts" type="text" placeholder="Enter your fullName here...">
     </div>
 
-    <div class="cont_2">
+    <div class="cont_2 ">
         <p>
             Email:
         </p>
-        <input class="inp_1" type="email" name="" id="" placeholder="Enter your email adress here...">
+        <input class="inp_3 inpts" type="email" name="" id="" placeholder="Enter your email adress here...">
 
     </div>
     <div class="cont_2">
         <p>Detail:</p>
-        <textarea class="inp_1" name="" id="" cols="30" rows="10" placeholder="Enter your detail here..."></textarea>
+        <textarea class="inp_4 " name="" id="" cols="30" rows="10" placeholder="Enter your detail here..."></textarea>
 
     </div>
     <button class="btn">
